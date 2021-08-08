e13e6855240488aa96d0d9fd8e57b636734e640a
BerryGod <84345559+BerryGod@users.noreply.github.com>
2021-08-08 16:00:11 +0200

    Update BerryHub.lua

--- BerryHub.lua
+++ BerryHub.lua
@@ -195,16 +195,11 @@ if game.PlaceId == 6284583030 then
 
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/AikaV3rm/UiLib/master/Lib.lua')))()
 
-local w = library:CreateWindow("A") -- Creates the window
+local w = library:CreateWindow("BerryHub") -- Creates the window
 
-local b = w:CreateFolder("B") -- Creates the folder(U will put here your buttons,etc)
+local b = w:CreateFolder("Pet Sim X") -- Creates the folder(U will put here your buttons,etc)
 
-b:Label("Pretty Useless NGL",{
-    TextSize = 25; -- Self Explaining
-    TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
-    BgColor = Color3.fromRGB(69,69,69); -- Self Explaining
-    
-}) 
+
 
 b:Button("Unlock All Gamepass",function()
     local gmppath = require(game:GetService("ReplicatedStorage").Framework.Modules.Client["5 | Gamepasses"])
